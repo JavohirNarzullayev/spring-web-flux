92065eb9b2031815998352f9a7bc4007fcdb01cd
Javohir <narzullayevj99@gmail.com>
2023-04-11 15:20:51 +0500

    change

--- src/org/example/controller/SseEmitterController.java
+++ src/org/example/controller/SseEmitterController.java
@@ -5,11 +5,15 @@ package org.example.controller;/*
 
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+import reactor.core.publisher.Flux;
 
 import java.io.IOException;
+import java.time.Duration;
+import java.time.LocalTime;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -53,11 +57,25 @@ public class SseEmitterController {
         service.shutdown();
         return sseEmitter;
     }
+
     private void randomDelay() {
         try {
             Thread.sleep(1000);
         } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
         }
+    }
+
+    @GetMapping(path = "/stream-flux", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<String> streamFlux() {
+
+        return Flux.interval(Duration.ofSeconds(1))
+                .map(sequence -> "Flux - " + LocalTime.now().toString());
+    }
+
+    public static void main(String[] args) {
+        Flux<String> just = Flux.just("Helklo", "World");
+        just.map(String::toUpperCase).filter(s -> s.startsWith("H"))
+                .subscribe(System.out::println);
     }
 }
